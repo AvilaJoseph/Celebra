c8f75fca7881420ab0c2ab73482c36d11aec4140
Joseph Avila <avilajoseph2021@gmail.com>
2025-04-23 15:14:33 -0500

    Implementación de las card y su diseño

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import 'gridstack/dist/gridstack.min.css';
+
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
