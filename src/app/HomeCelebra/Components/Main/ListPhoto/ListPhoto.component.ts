5eced87b7b47dd08d6c0889b392f215e77e06464
Joseph Avila <avilajoseph2021@gmail.com>
2025-04-23 12:09:36 -0500

    Modal en el ListButtm

--- src/app/HomeCelebra/Components/Main/ListPhoto/ListPhoto.component.ts
+++ src/app/HomeCelebra/Components/Main/ListPhoto/ListPhoto.component.ts
@@ -1,10 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'app-list-photo',
-  imports: [],
+  selector: 'Main-Celebre-ListPhoto',
+  imports: [
+    CommonModule
+  ],
   templateUrl: './ListPhoto.component.html',
   styleUrl: './ListPhoto.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListPhotoComponent { }
+export class ListPhotoComponent {
+
+  showStockModal = false;
+  selectedOption: string = '';
+
+  selectOption(option: string): void {
+    this.selectedOption = option;
+    this.showStockModal = false;
+  }
+
+
+}
--- src/app/HomeCelebra/Components/Main/Main.component.ts
+++ src/app/HomeCelebra/Components/Main/Main.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ListPhotoComponent } from "./ListPhoto/ListPhoto.component";
 
 @Component({
   selector: 'Home-Celebra-Main',
-  imports: [],
+  imports: [ListPhotoComponent],
   templateUrl: './Main.component.html',
   styleUrl: './Main.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
