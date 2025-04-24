438b36e3c3a9bb2d78dfaa9bf22ac3f5a1f54aac
AvilaJoseph <avilajoseph2021@gmail.com>
2025-04-23 22:04:42 -0500

    Creación del componente Cards

--- src/app/HomeCelebra/Components/Main/CardPhoto/CardPhoto.component.ts
+++ src/app/HomeCelebra/Components/Main/CardPhoto/CardPhoto.component.ts
@@ -1,10 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+// CardPhoto.component.ts
+import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-photo',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './CardPhoto.component.html',
   styleUrl: './CardPhoto.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CardPhotoComponent { }
+export class CardPhotoComponent {
+  @Input() itemNumber: number = 0;
+  @Input() itemSize: string = 'normal-size';
+  @Input() backgroundColor: string = '#ffffff';
+  @Input() content: string = '';
+  
+  showStockModal = false;
+}
