edf7e154f2132ae2d4d40359c7b65fe7278564a6
AvilaJoseph <avilajoseph2021@gmail.com>
2025-05-11 21:50:41 -0500

    Implementación del modal

---
+++ src/app/Back-Celebre/Components/ModalMedia/ModalMedia.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-media',
+  imports: [],
+  templateUrl: './ModalMedia.component.html',
+  styleUrl: './ModalMedia.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ModalMediaComponent { }
